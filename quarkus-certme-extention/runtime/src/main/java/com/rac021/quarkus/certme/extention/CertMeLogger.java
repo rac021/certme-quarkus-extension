627c8a57c802bbf6309306f2105980b4e0cceac3
rachid.yahiaoui <r.yahiaoui.21@gmail.com>
2021-03-03 14:58:55 +0100

    fix quarkus_duplicate_entry

--- quarkus-certme-extention/runtime/src/main/java/com/rac021/quarkus/certme/extention/CertMeLogger.java
+++ quarkus-certme-extention/runtime/src/main/java/com/rac021/quarkus/certme/extention/CertMeLogger.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.Level ;
  */
 public class CertMeLogger {
     
-    public static Level checkLog( String level )           {
+    public static Level checkLog( String level )            {
 
        Level toLevel = Level.toLevel( level.toUpperCase() ) ;
        System.out.println( "\nRetained LOG LEVEL  "         + 
